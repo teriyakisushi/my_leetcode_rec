d11fea2a38bbb49dfa6e02eb62b6c54bec6f66e1
teriyakisushi <yrahkz@outlook.com>
2024-11-12 21:58:16 +0800

    2024-11-12

--- easy/lc1_TwoSum.go
+++ easy/lc1_TwoSum.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+/*
+@ Problem: Leetcode 1. Two Sum
+@ Solution: 哈希表
+@ Time Complexity: O(n)
+@ Detail:
+    考虑到题目要求返回的是下标，很容易想到用哈希表来存储遍历过的元素。
+    遍历数组，对于每个元素num，判断target-num是否在哈希表中，如果在，返回两个元素的下标。
+    如果不在，将num存入哈希表中。
+*/
+
+
 func twoSum(nums []int, target int) []int {
     hashMap := make(map[int]int)
     for i, num := range nums {
